66f70379890507a712d09dd5b767c186763f500a
EfficiencySimplicity <joshua.ward@fastmail.com>
2026-07-28 10:29:03 -0400

    got an iter-cells method added

--- Scripts/map_layer.gd
+++ Scripts/map_layer.gd
@@ -12,7 +12,7 @@ var world_origin: Vector2
 ## The size in world space of each cell in the MapLayer
 var cell_size: int = 64
 
-func _init(_size: Vector2i, fill: bool = false):
+func _init(_size: Vector2i = Vector2i(1, 1), fill: bool = false):
 	size = _size
 	data = BitMap.new()
 	data.create(_size)
@@ -23,6 +23,10 @@ static func from_rect(rect: Rect2, _cell_size: int = 64, fill: bool = false) -> 
 	var map_layer = MapLayer.new(rect.size / _cell_size, fill)
 	map_layer.world_origin = rect.position
 	return map_layer
+	
+# This could be a Rect2i...
+func get_world_rect() -> Rect2:
+	return Rect2(world_origin, size * cell_size)
 	
 static func from_layers(layers: Array) -> MapLayer:
 	var bounds = get_group_bounds(layers)
@@ -43,71 +47,10 @@ static func get_group_bounds(layers: Array[MapLayer]) -> Rect2:
 		rect = rect.merge(layer.get_world_rect())
 	return rect
 	
-# This could be a Rect2i...
-func get_world_rect() -> Rect2:
-	return Rect2(world_origin, size * cell_size)
-	
-## Returns the top left corner of the specified cell in world space
-func cell_to_world(pos: Vector2i):
-	return world_origin + Vector2(pos * cell_size)
-
-## Gets an value from the MapLayer
-func g(pos: Vector2i) -> bool:
-	return data.get_bitv(pos)
-
-## Sets a value in the MapLayer
-func s(pos: Vector2i, value: bool):
-	data.set_bitv(pos, value)
-	
-## Stamps this MapLayer on another, overwriting all values within its area
-func stamp_on(other: MapLayer, pos: Placement):
-	for y in range(size.y):
-		for x in range(size.x):
-			# we can just add the two since down is +y in GD
-			# of course if up was +y the whole room would be
-			# in quadrant I so it doesn't matter anyway, right?
-			other.s(pos.map_vector2i(Vector2i(x, y)), g(Vector2i(x, y)))
-			
-## Places this MapLayer on another, only writing down 1s
-func place_on(other: MapLayer, pos: Placement):
-	for y in range(size.y):
-		for x in range(size.x):
-			# we can just add the two since down is +y in GD
-			# of course if up was +y the whole room would be
-			# in quadrant I so it doesn't matter anyway, right?
-			if g(Vector2i(x, y)):
-				other.s(pos.map_vector2i(Vector2i(x, y)), true)
-			
-## Returns true if the MapLayers contain equal values starting at the specified position
-func matches(other: MapLayer, pos: Placement) -> bool:
-	for y in range(size.y):
-		for x in range(size.x):
-			# we can just add the two since down is +y in GD
-			# of course if up was +y the whole room would be
-			# in quadrant I so it doesn't matter anyway, right?
-			if other.g(pos.map_vector2i(Vector2i(x, y))) != g(Vector2i(x, y)):
-				return false
-	return true
-	
-## Returns true if the MapLayers have at least 1 cell where they're both true
-func collides(other: MapLayer, pos: Placement) -> bool:
-	for y in range(size.y):
-		for x in range(size.x):
-			if other.g(pos.map_vector2i(Vector2i(x, y))) and g(Vector2i(x, y)):
-				return true
-	return false
-	
-## Returns true if the MapLayers do not collide with any 1s
-func doesnt_collide(other: MapLayer, pos: Placement) -> bool:
-	return !collides(other, pos)
-	
-## Returns true if wherever the MapLayer is true, the other is also
-func overlaps(other: MapLayer, pos: Placement) -> bool:
-	for y in range(size.y):
-		for x in range(size.x):
-			if g(Vector2i(x, y)) and not other.g(pos.map_vector2i(Vector2i(x, y))):
-					return false
-	return true
+# The cell size could be a custom Node Type; say... PlacementLayer? could be different from MapLayer or something
+static func from_collision_shape(shape: CollisionShape2D, _cell_size: int) -> MapLayer:
+	var bounding_rect = Rect2i(Vector2i(shape.global_position - shape.shape.size / 2), shape.shape.size)
+	return MapLayer.from_rect(bounding_rect, _cell_size, 1)
 	
 ## The MapLayer will be 0 (false) wherever it is outside the room
 static func from_area2d(area: Area2D, _cell_size: int = 64) -> MapLayer:
@@ -126,6 +69,63 @@ static func from_area2d(area: Area2D, _cell_size: int = 64) -> MapLayer:
 			
 	return MapLayer.from_layers(minis)
 
+
+## Gets an value from the MapLayer
+func g(pos: Vector2i) -> bool:
+	return data.get_bitv(pos)
+
+## Sets a value in the MapLayer
+func s(pos: Vector2i, value: bool):
+	data.set_bitv(pos, value)
+
+## Returns the top left corner of the specified cell in world space
+func cell_to_world(pos: Vector2i):
+	return world_origin + Vector2(pos * cell_size)
+	
+## Stamps this MapLayer on another, overwriting all values within its area
+func stamp_on(other: MapLayer, pos: Placement):
+	iter_cells(func(xy): other.s(pos.map_vector2i(xy), g(xy)))
+			
+## Places this MapLayer on another, only writing down 1s
+func place_on(other: MapLayer, pos: Placement):
+	iter_cells(
+		func(xy):
+			if g(xy):
+				other.s(pos.map_vector2i(xy), true)
+	)
+
+## Returns true if the MapLayers contain equal values starting at the specified position
+func matches(other: MapLayer, pos: Placement) -> bool:
+	return iter_cells(
+		(func(xy):
+			if other.g(pos.map_vector2i(xy)) != g(xy):
+				return false),
+		true
+	)
+	
+## Returns true if the MapLayers have at least 1 cell where they're both true
+func collides(other: MapLayer, pos: Placement) -> bool:
+	return iter_cells(
+		(func(xy): 
+			if other.g(pos.map_vector2i(xy)) and g(xy):
+				return true),
+		false
+	)
+	
+## Returns true if the MapLayers do not collide with any 1s
+func doesnt_collide(other: MapLayer, pos: Placement) -> bool:
+	return !collides(other, pos)
+	
+## Returns true if wherever the MapLayer is true, the other is also
+func overlaps(other: MapLayer, pos: Placement) -> bool:
+	return iter_cells(
+		(func(xy):
+			if g(xy) and not other.g(pos.map_vector2i(xy)):
+				return false),
+		true
+	)
+	
+
 func inverted() -> MapLayer:
 	var new_data = BitMap.new()
 	new_data.create(size)
@@ -133,7 +133,7 @@ func inverted() -> MapLayer:
 		for x in size.x:
 			new_data.set_bit(x, y, !data.get_bit(x, y))
 			
-	var new_layer = MapLayer.new(Vector2(0, 0))
+	var new_layer = MapLayer.new()
 	new_layer.data = new_data
 	new_layer.size = size
 	new_layer.cell_size = cell_size
@@ -141,11 +141,16 @@ func inverted() -> MapLayer:
 	
 	return new_layer
 	
-
-# The cell size could be a custom Node Type; say... PlacementLayer? could be different from MapLayer or something
-static func from_collision_shape(shape: CollisionShape2D, _cell_size: int) -> MapLayer:
-	var bounding_rect = Rect2i(Vector2i(shape.global_position - shape.shape.size / 2), shape.shape.size)
-	return MapLayer.from_rect(bounding_rect, _cell_size, 1)
+	
+func iter_cells(f: Callable, default = null):
+	for y in size.y:
+		for x in size.x:
+			var v = f.call(Vector2i(x, y))
+			if v != null:
+				return v
+	if default != null:
+		return default
+	
 	
 # https://forum.godotengine.org/t/how-to-make-a-class-printable/25991/4
 func _to_string() -> String:
